2326fd6be9947c97c6971b7590ef6058d52e6d50
Syeda Gohar Ali Naqvi <50008082+goharnaqvi@users.noreply.github.com>
2019-06-30 21:39:26 +0500

    php syntex and variables

---
+++ united.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<?php
+		echo "My first PHP script! <br>";
+		echo "gohar.<br>";
+//for comment
+		$x = 5 /* + 15 */ + 5;
+		echo $x."<br>";
+		$x = 5+5+5;
+		echo $x ."<br>";
+		$color="red";
+		echo "my car is "
+		.$color."<br>";
+		$txt ="hello word";
+		$x=5;
+		$y=10.5;
+
+		echo "$txt";
+		echo "<br>";
+		echo "$x";
+		echo "<br>";
+		echo "$y";
+		echo "<br>";
+		$txt= "w3school.com";
+		echo "i love "
+		.$txt. "!";
+		echo "<br>";
+		$x = 5;
+		$y = 4;
+		echo $x + $y;
+		
+	?>
+
+
+</body>
+</html>
