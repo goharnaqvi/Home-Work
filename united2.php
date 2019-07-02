753572c93719a701ba714eda55d2759d80a7b4a6
Syeda Gohar Ali Naqvi <50008082+goharnaqvi@users.noreply.github.com>
2019-07-02 23:49:41 +0500

    echo/print

---
+++ united2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+		<body>
+				<?php
+
+				$txt1 = "gohar ali";
+				$txt2 = "Gohar ";
+
+				$x = 6;
+				$y = 10;
+
+				echo "<h2>" . $txt1 . "</h2>";
+				echo "My name is  " . $txt2 . "<br>";
+
+				echo "$x + $y";
+
+				?>
+		</body>
+</html>
