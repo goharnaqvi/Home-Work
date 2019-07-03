912d1d3197fc69781b0597e0f5b85ebdad872b9d
Syeda Gohar Ali Naqvi <50008082+goharnaqvi@users.noreply.github.com>
2019-07-03 23:39:19 +0500

    php strings

---
+++ strings.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+		<?php
+
+				echo strlen("hello world");
+				//Count The Number of Words in a String
+				echo "<br>";
+				echo str_word_count("hello world");
+
+				//Reverse a String
+				echo "<br>";
+				echo strrev("hello world");
+				//Search For a Specific Text Within a String
+				echo "<br>";
+
+				echo strpos("Hello world!", "world");
+		?>
+
+</body>
+</html>
